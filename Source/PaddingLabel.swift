b7bd6c3d3330b1ba30ca6f74729fbd064c446001
Tom.Huang <hzlhu.dargon@gmail.com>
2018-05-02 13:52:01 +0800

    add bottomBorder to PaddingLabel

--- Source/PaddingLabel.swift
+++ Source/PaddingLabel.swift
@@ -1,6 +1,13 @@
 import UIKit
+import SpriteKit
 
 public class PaddingLabel: UILabel {
+
+    public var bottomBorderColor: UIColor? {
+        didSet {
+            self.borderBottom.backgroundColor = self.bottomBorderColor
+        }
+    }
 
     public var padding: UIEdgeInsets = UIEdgeInsets.zero {
         didSet {
@@ -8,7 +15,33 @@ public class PaddingLabel: UILabel {
         }
     }
 
+    let borderBottom = UIView()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initViews()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initViews()
+    }
+
+    public override func updateConstraints() {
+        super.updateConstraints()
+        self.borderBottom.snp.remakeConstraints { (make) in
+            make.left.right.equalTo(self)
+            make.top.equalTo(self.snp.bottom).offset(-1)
+            make.height.equalTo(1)
+        }
+    }
+
     public override func drawText(in rect: CGRect) {
         super.drawText(in: UIEdgeInsetsInsetRect(rect, self.padding))
+    }
+
+    func initViews() {
+        self.borderBottom.backgroundColor = self.backgroundColor
+        self.addSubview(self.borderBottom)
     }
 }
