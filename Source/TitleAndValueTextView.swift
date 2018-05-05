628170b9a4019485e61bde10918729d7f3a7e82c
Tom.Huang <hzlhu.dargon@gmail.com>
2018-05-05 15:05:40 +0800

    add required tag for TitleAndValueTextView

--- Source/TitleAndValueTextView.swift
+++ Source/TitleAndValueTextView.swift
@@ -4,6 +4,7 @@ import SnapKit
 open class TitleAndValueTextView: BorderBottomView {
 
     open var titleWidth: CGFloat?
+    open var isRequired: Bool = false
     open var leftPadding: CGFloat = 16
     open var rightPadding: CGFloat = 16
 
@@ -55,18 +56,30 @@ open class TitleAndValueTextView: BorderBottomView {
 
     let titleLabel = UILabel()
     let valueLabel = UILabel()
+    let requiredLabel = UILabel()
 
     open override func updateConstraints() {
         super.updateConstraints()
+        var titleLeftPadding: CGFloat
+        if self.isRequired {
+            self.requiredLabel.snp.remakeConstraints { (make) in
+                make.left.equalTo(self).offset(self.leftPadding)
+                make.width.equalTo(6)
+                make.top.bottom.equalTo(self)
+            }
+            titleLeftPadding = self.leftPadding + 8
+        } else {
+            titleLeftPadding = self.leftPadding
+        }
         if let titleWidth = self.titleWidth {
             self.titleLabel.snp.remakeConstraints { (make) in
-                make.left.equalTo(self).offset(self.leftPadding)
+                make.left.equalTo(self).offset(titleLeftPadding)
                 make.width.equalTo(titleWidth)
                 make.top.bottom.equalTo(self)
             }
         } else {
             self.titleLabel.snp.remakeConstraints { (make) in
-                make.left.equalTo(self).offset(self.leftPadding)
+                make.left.equalTo(self).offset(titleLeftPadding)
                 make.right.equalTo(self.snp.centerX)
                 make.top.bottom.equalTo(self)
             }
@@ -84,6 +97,11 @@ open class TitleAndValueTextView: BorderBottomView {
 
     override open func initViews() {
         self.backgroundColor = UIColor.white
+
+        self.requiredLabel.font = UIFont.systemFont(ofSize: 14)
+        self.requiredLabel.textColor = UIColor.red
+        self.requiredLabel.text = "*"
+        self.addSubview(self.requiredLabel)
 
         self.titleLabel.text = self.title
         self.titleLabel.font = self.titleFont
