2dbd8e9655cca31010bf33077ede6c2fc1adccc1
Tom.Huang <hzlhu.dargon@gmail.com>
2018-05-10 13:55:57 +0800

    Revert "remove unuseful logic"
    This reverts commit 0a590e9f7bddcd02406fd488d2e589f0ece2ea78.

--- Source/PaddingTextField.swift
+++ Source/PaddingTextField.swift
@@ -1,11 +1,35 @@
 import UIKit
+import SpriteKit
 
 open class PaddingTextField: UITextField {
+    
+    open var bottomBorderColor: UIColor? {
+        didSet {
+            self.borderBottom.backgroundColor = self.bottomBorderColor
+        }
+    }
 
     open var padding: UIEdgeInsets = UIEdgeInsets.zero
 
-    open override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    let borderBottom = UIView()
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initViews()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initViews()
+    }
+    
+    open override func updateConstraints() {
+        super.updateConstraints()
+        self.borderBottom.snp.remakeConstraints { (make) in
+            make.left.right.equalTo(self)
+            make.top.equalTo(self.snp.bottom).offset(-1)
+            make.height.equalTo(1)
+        }
     }
     
     open override func textRect(forBounds bounds: CGRect) -> CGRect {
@@ -18,5 +42,10 @@ open class PaddingTextField: UITextField {
 
     open override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return UIEdgeInsetsInsetRect(bounds, self.padding)
+    }
+
+    open func initViews() {
+        self.borderBottom.backgroundColor = self.backgroundColor
+        self.addSubview(self.borderBottom)
     }
 }
